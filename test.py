c1e38f06e1764b52a968f80cefc12952956944fe
Juyounge-e <ghn09081@gmail.com>
2025-05-31 22:45:47 +0900

    ⚡️: env 등록 라인 추가

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
-import gymnasium
+import gymnasium as gym
 import highway_env
 from matplotlib import pyplot as plt
 
 
-env = gymnasium.make('roundabout-v0', render_mode='rgb_array')
+# env = gym.make("custom-mixed-road-v0", render_mode="rgb_array")
+env = gym.make("merge-v0", render_mode="rgb_array")
+
 env.reset()
+
+road = env.unwrapped.road
 for _ in range(10):
     action = env.unwrapped.action_type.actions_indexes["IDLE"]
     obs, reward, done, truncated, info = env.step(action)
